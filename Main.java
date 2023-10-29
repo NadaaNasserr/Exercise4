6b378d3766690e173eaf9f317282036c20383f1e
NadaaNasserr <148685934+NadaaNasserr@users.noreply.github.com>
2023-10-29 18:34:20 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,102 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+
+
+                //1
+
+//        int[] array = {50, -20, 0, 30, 40, 60, 10};
+//        if(array[0] == array[array.length-1]){
+//            System.out.println("true");
+//        }
+//        else  System.out.println("false");
+
+
+              //2
+//        int[] array = {1, 4, 17, 7, 25, 3, 100};
+//        int sum =0;
+//        int average=0;
+//        for (int i =0; i< array.length; i++){
+//            sum+= array[i];
+//        }
+//        average = sum / array.length;
+//        System.out.println("The average is " + average);
+//        for (int i =0; i< array.length; i++){
+//            if(average <= array[i])
+//                System.out.print(array[i] + " ");
+//        }
+
+
+               //3
+//        int[] array = {20, 30, 40};
+//        int largervalue=0;
+//        for (int i = 0; i< array.length; i++){
+//            if (array[i] > largervalue)
+//                largervalue = array[i];
+//
+//        }
+//        System.out.println(largervalue);
+
+
+
+
+
+            //4
+//       int[] array = {20, 30, 40};
+//        ArrayList<Integer> array1 = new ArrayList<Integer>();
+//        int swap;
+//        for (int i = 0; i< array.length; i++){
+//            System.out.println(array[i]);
+//        }
+//
+//        swap = array[0];
+//        array[0] = array[array.length-1];
+//        array[array.length-1] = swap;
+//
+//        for (int i = 0; i< array.length; i++){
+//           array1.add(array[i]);
+//
+//        }
+//        System.out.println(array1);
+
+
+             //5
+//        int[] array = {2, 3, 40, 1, 5, 9, 4, 10, 7};
+//        ArrayList<Integer> array1 = new ArrayList<Integer>();
+//
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] % 2 != 0) {
+//                array1.add(array[i]);
+//            }
+//        }
+//
+//        for (int j = 0; j < array.length; j++) {
+//            if (array[j] % 2 == 0) {
+//                array1.add(array[j]);
+//            }
+//        }
+//        System.out.println(array1);
+
+
+           //6
+//        int[] array1 = {2, 3, 6, 6, 4};
+//        int[] array2 = {2, 3, 6, 6, 4};
+//
+//        for (int i =0 ; i< array1.length; i++)
+//                if(array1[i] == array2[i]) {
+//                    System.out.println("true");
+//                    break;
+//                }
+//
+//                else
+//                    System.out.println("false");
+
+
+    }
+}
